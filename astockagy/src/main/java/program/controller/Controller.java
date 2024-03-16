029af36f5e904ef885dd38bd1fe765b425c63f1f
connortkhda <65467224+connortkhda@users.noreply.github.com>
2024-03-16 22:47:58 +0000

    CSVs exported from python script

--- astockagy/src/main/java/program/controller/Controller.java
+++ astockagy/src/main/java/program/controller/Controller.java
@@ -1,6 +1,8 @@
 package program.controller;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import program.scene.MainScene;
 
 public class Controller {
@@ -39,12 +41,29 @@ public class Controller {
   }
 
   public void createStockGraph() {
+    //try {
+    //   @SuppressWarnings({"deprecation", "unused"})
+    //   Process runtime = Runtime.getRuntime().exec("python src\\main\\java\\program\\model\\stockData.py AMD");
+    // } catch (IOException e) {
+    //   System.out.println("Something went wrong with the stock grpah");
+    // }
+
     try {
-      @SuppressWarnings({"deprecation", "unused"})
-      Process runtime = Runtime.getRuntime().exec("python src\\main\\java\\program\\model\\stockData.py AMD");
-    } catch (IOException e) {
-      System.out.println("Something went wrong with the stock grpah");
+      String filePath = "src\\main\\java\\program\\model\\stockData.py";      
+      ProcessBuilder pb = new ProcessBuilder().command("python", filePath, "AMD");        
+      Process p = pb.start(); 
+      BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+      StringBuilder buffer = new StringBuilder();     
+      String line = null;
+      while ((line = in.readLine()) != null){           
+        buffer.append(line);
+      }
+      int exitCode = p.waitFor();
+      System.out.println("Value is: "+buffer.toString());                
+      System.out.println("Process exit value:"+exitCode);        
+      in.close();
+    } catch (Exception e) {
+      System.out.println("Something went wrong reading python data");
     }
   }
-
 }
--- astockagy/target/classes/program/model/stockData.py
+++ astockagy/target/classes/program/model/stockData.py
@@ -7,7 +7,16 @@ import matplotlib.pyplot as plt
 #print("Called")
 stock = sys.argv[1]
 data = yf.download(stock,"2019-3-22")["Adj Close"]
-print(data)
-plt.plot(data)
+
+#print(data)
+
+graph = plt.plot(data)
 #plt.show()
+
+#WHEN RUNNING FROM JAVA -----------------------------------------------------------------------------------------------
+data.to_csv("src/main/resources/data/" + stock + "Data.csv")
 plt.savefig("src/main/resources/images/" + stock + ".png")
+
+#WHEN RUNNING FROM TERMINAL -----------------------------------------------------------------------------------------------
+# data.to_csv("astockagy/src/main/resources/data/" + stock + "Data.csv")
+# plt.savefig("astockagy/src/main/resources/images/" + stock + ".png")
