ca9dcbc6ce57c48d17766ac3720c3956f1259ae5
connortkhda <65467224+connortkhda@users.noreply.github.com>
2024-03-16 15:23:21 +0000

    Added logo scrappily to side bar

--- astockagy/src/main/java/program/scene/MainScene.java
+++ astockagy/src/main/java/program/scene/MainScene.java
@@ -2,12 +2,18 @@ package program.scene;
 
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import program.ui.SideMenu;
 
 public class MainScene extends StackPane {
 
   public MainScene() {
     HBox mainDisplay = new HBox();
-    
+
+    SideMenu sideMenu = new SideMenu();
+
+    mainDisplay.getChildren().add(sideMenu);
+
+    getChildren().add(mainDisplay);
   }
   
 }
--- astockagy/src/main/java/program/ui/SideMenu.java
+++ astockagy/src/main/java/program/ui/SideMenu.java
@@ -1,7 +1,28 @@
 package program.ui;
 
+import javafx.geometry.Insets;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 public class SideMenu extends VBox {
   
+  public SideMenu() {
+    setSpacing(5);
+    setBackground(new Background(new BackgroundFill(Color.rgb(144, 65, 193), CornerRadii.EMPTY, Insets.EMPTY)));
+    ImageView logo = new ImageView(new Image(this.getClass().getResource("/images/logo.png").toExternalForm()));
+    logo.setFitHeight(100);
+    logo.setFitWidth(100);
+    getChildren().add(logo);
+    //setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2))));
+
+  }
 }
