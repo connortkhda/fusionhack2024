de240f34d57ad8b3c4ae223215875f76400aa228
connortkhda <65467224+connortkhda@users.noreply.github.com>
2024-03-17 01:51:59 +0000

    Transcribed mercury retrograde dates

--- astockagy/src/main/java/program/controller/Controller.java
+++ astockagy/src/main/java/program/controller/Controller.java
@@ -103,4 +103,11 @@ public class Controller {
 
     return parseDate(latest.get(Calendar.YEAR) + "-" + latest.get(Calendar.MONTH) + "-" + latest.get(Calendar.DAY_OF_MONTH));
   }
+
+  public static Date[] getMercuryRetrogradeDates() {
+    Date[] dates = {parseDate("2015-03-05"),parseDate("2015-07-07"),parseDate("2015-10-31"),parseDate("2020-02-16"),parseDate("2020-06-17"),
+    parseDate("2020-10-13"),parseDate("2021-01-30"),parseDate("2021-05-29"),parseDate("2021-09-27"),parseDate("2022-01-14"),parseDate("2022-05-10"),
+    parseDate("2022-09-09"),parseDate("2022-12-29"),parseDate("2023-04-21"),parseDate("2023-08-23"),parseDate("2023-12-13")};
+    return dates;
+  }
 }
